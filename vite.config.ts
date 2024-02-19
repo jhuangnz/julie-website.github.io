b11565aec4f0bff773baba66cf712f6e9dd60294
Julie Huang <julie@juliehuang.co.nz>
2024-02-19 16:32:07 +1100

    Set base URL as github page

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/julie-website.github.io/',
   plugins: [
     vue(),
     vueJsx(),
